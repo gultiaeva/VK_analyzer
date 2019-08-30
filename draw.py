88d2622f85d0fd8032f6738295d606cfd6c90473
gultiaeva <angultiaev@gmail.com>
2019-08-30 23:39:32 +0300

    minor fix

--- draw.py
+++ draw.py
@@ -1,4 +1,4 @@
-from os import mkdir
+from os import makedirs
 from collections import Counter
 import numpy as np
 import pandas as pd
@@ -7,10 +7,14 @@ import seaborn as sns
 import wordcloud
 
 
+class NotDrawnYetException(Exception):
+    pass
+
 class Drawer():
     def __init__(self, data, persons=None):
         self.df = data
         self.names = self.select_persons() if persons is None else persons
+        self.is_drawn = False
 
     def select_persons(self):
         return self.df['name'].unique()
@@ -20,24 +24,22 @@ class Drawer():
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16, 9))
         ax.pie(chars_per_person.values, labels=chars_per_person.index)
         ax.set_title('Распределение количества отправленных символов')
-        # return fig
-        self._save_figure(fig, 'symbols_per_person.png')
+        return fig
 
     def draw_messages_per_person(self):
         msg_per_person = self.df.groupby('name')['message'].count()
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16, 9))
         ax.pie(msg_per_person.values, labels=msg_per_person.index)
         ax.set_title('Распределение количества отправленных сообщений')
-        # return fig
-        self._save_figure(fig, 'messages_per_person.png')
+        return fig
+        
     
-    def draw_emojis_per_person(self):
+    def draw_emotes_per_person(self):
         emojis_per_person = self.df.groupby('name')['emojis'].sum()
         fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16, 9))
         ax.pie(emojis_per_person.values, labels=emojis_per_person.index)
         ax.set_title('Распределение количества отправленных emoji')
-        # return fig
-        self._save_figure(fig, 'emotes_per_person.png')
+        return fig
         
     def draw_most_common_words(self):
         wordcount = {}
@@ -51,7 +53,7 @@ class Drawer():
                         tmp.append(word)
             wordcount[name] = Counter(tmp)
 
-        #plots = []
+        plots = []
         for name in self.names:
             mc = np.array(wordcount[name].most_common(30))
             fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(16, 9))
@@ -60,13 +62,12 @@ class Drawer():
             ax.set_ylabel('Частота')
             ax.set_title('Частота употребления слов для ' + name)
             ax.xaxis.set_tick_params(rotation=90)
-            #plots.append(fig)
-            self._save_figure(fig, 'common_words ' + name + '.png')
-        #return plots
+            plots.append((fig, 'common_words' + name + '.png'))
+        return plots
             
     def draw_wordclouds_each_person(self):
-        # clouds = []
-        mkdir('./plots/wordclouds')
+        clouds = []
+        makedirs('./plots/wordclouds', exist_ok=True)
         for name in self.names:
             tmp = ' '.join(msg for msg in self.df[self.df['name'] == name]['message'] 
                            if 'пересланное' not in msg 
@@ -77,10 +78,10 @@ class Drawer():
                                         height=1080, 
                                         background_color='white').generate(tmp)
             
-            cloud.to_file('./plots/wordclouds/cloud ' + name + '.png')
-        # return clouds
+            clouds.append((cloud, './plots/wordclouds/cloud ' + name + '.png'))
+        return clouds
     
-    def draw_wordclouds_all(self):
+    def draw_wordcloud_all(self):
         tmp = ' '.join(msg for msg in self.df['message'] 
                        if 'пересланное' not in msg 
                        and 'attachment' not in msg 
@@ -89,8 +90,7 @@ class Drawer():
         cloud = wordcloud.WordCloud(width=1920, 
                                     height=1080, 
                                     background_color='white').generate(tmp)
-        cloud.to_file('./plots/wordclouds/cloud_all_users.png')
-
+        return cloud
 
     def draw_messages_per_hour_distribution(self):
         dist_hours = self.df.groupby('hour')['message'].count()
@@ -99,8 +99,7 @@ class Drawer():
         ax.set_xlabel('Часы')
         ax.set_ylabel('Количество сообщений')
         ax.set_title('Распределение сообщений по часам в сутках')
-        # return fig
-        self._save_figure(fig, 'messages_per_hour.png')
+        return fig
         
     def draw_messages_count_heatmap(self):
         crosstab = pd.pivot_table(self.df, 
@@ -117,20 +116,44 @@ class Drawer():
         ax.set_ylabel('Год/месяц')
         ax.set_title('Тепловая карта сообщений')
         ax.set_aspect('equal')
-        # return fig
-        self._save_figure(fig, 'message_heatmap.png')
+        return fig
 
     def _save_figure(self, figure, fname):
         figure.savefig('./plots/' + fname, dpi=300,
                         bbox_inches='tight', pad_inches=0.5)
 
     def draw_all(self):
-        self.draw_messages_per_person()
-        self.draw_symbols_per_person()
-        self.draw_emojis_per_person()
-        self.draw_most_common_words()
-        self.draw_messages_per_hour_distribution()
-        self.draw_wordclouds_each_person()
-        self.draw_wordclouds_all()
-        self.draw_messages_count_heatmap()
+        self.messages_per_person = self.draw_messages_per_person()
+        self.symbols_per_person = self.draw_symbols_per_person()
+        self.emotes_per_person = self.draw_emotes_per_person()
+        self.most_common_words = self.draw_most_common_words()
+        self.messages_per_hour_dist = self.draw_messages_per_hour_distribution()
+        self.wordclouds_each_person = self.draw_wordclouds_each_person()
+        self.wordcloud_all = self.draw_wordcloud_all()
+        self.messages_count_heatmap = self.draw_messages_count_heatmap()
+        self.is_drawn = True
+
+    def save_all(self):
+        if not self.is_drawn:
+            raise NotDrawnYetException('Not yet!')
+
+        self._save_figure(self.symbols_per_person,
+                          'symbols_per_person.png')
+        self._save_figure(self.messages_per_person,
+                          'messages_per_person.png')
+        self._save_figure(self.emotes_per_person,
+                          'emotes_per_person.png')
+        self._save_figure(self.messages_per_hour_dist,
+                          'messages_per_hour.png')
+        self._save_figure(self.messages_count_heatmap,
+                          'message_heatmap.png')
+        for fig, fname in self.most_common_words:
+            self._save_figure(fig, fname)
         
+        for fig, fname in self.wordclouds_each_person:
+            fig.to_file(fname)
+
+        self.wordcloud_all.to_file('./plots/wordclouds/cloud_all_users.png')
+        
+        
+
--- message_analyzer.py
+++ message_analyzer.py
@@ -12,6 +12,17 @@ class MessageAnalyzer():
         prepared_messages = Preparer(self.extracted).prepare()
         drawer = Drawer(prepared_messages)
         drawer.draw_all()
+        print('''
+        Your plots r done!
+        Would u like to save it? (y/n)
+        ''')
+        while True:
+            choice = input()
+            if choice in 'yY':
+                drawer.save_all()
+                break
+            elif choice in 'nN':
+                break
 
 
 if __name__ == '__main__':
